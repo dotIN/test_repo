ad9a4de72a6e3259d4e8f293246e79d68e88516c
r <you@example.com>
2017-12-12 11:05:29 +1300

    glogal shared lib

--- vars/buildReports.groovy
+++ vars/buildReports.groovy
@@ -1,10 +1,4 @@
-def call(body) {
-    // evaluate the body block, and collect configuration into the object
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-
+def call() {
     // now build, based on the configuration provided
     node {
             echo "Entering buildReports Global Lib"
@@ -12,6 +6,7 @@ def call(body) {
             // Compress and store reports
             // Sync logs
             echo "emailext attachmentsPattern: '**/cucumber-results-feature-overview.pdf', body: 'Attached report for ${BUILD_URL} [${JOB_NAME}] [${BUILD_NUMBER}]', subject: '${JOB_NAME} [${BUILD_NUMBER}]', to: 'test@email.com'" 
+	    echo "${BUILD_URL}"
             echo "Entering buildReports Global Lib"
     }
 }
