3649a294761c26f212f5889bea94644a63271247
r <you@example.com>
2017-12-12 11:08:50 +1300

    glogal shared lib

--- vars/buildReports.groovy
+++ vars/buildReports.groovy
@@ -12,6 +12,7 @@ def call(body) {
             // Compress and store reports
             // Sync logs
             echo "emailext attachmentsPattern: '**/cucumber-results-feature-overview.pdf', body: 'Attached report for ${BUILD_URL} [${JOB_NAME}] [${BUILD_NUMBER}]', subject: '${JOB_NAME} [${BUILD_NUMBER}]', to: 'test@email.com'" 
+	    echo "${BUILD_URL}"
             echo "Entering buildReports Global Lib"
     }
 }
